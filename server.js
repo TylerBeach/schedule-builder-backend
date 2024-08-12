e6826998b0eb4a4f208416d7f658712b300c12a3
TylerBeach <TylerAllenBeach@gmail.com>
2024-08-12 16:49:30 -0600

    Configure server for deployment

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/course/:department/:course', async (req, res) => {
 });
 
 
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Server started");
 });
 
