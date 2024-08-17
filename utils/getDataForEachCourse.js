75ee193d43af009eec70879ac251e930209dbd73
TylerBeach <TylerAllenBeach@gmail.com>
2024-08-16 23:02:44 -0600

    progress towards schedule variations

--- utils/getDataForEachCourse.js
+++ utils/getDataForEachCourse.js
@@ -1,6 +1,25 @@
-export default function getCourseData(courseArray) {
-    
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') }); 
 
-
-
+async function getCourseData(term, courseArray) {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    let cartData = [];
+    for (let course of courseArray) {
+        let [department, courseId] = course.split(" ");
+        let url = `${apiUrl}/course/${term}/${department}/${courseId}`;
+        await fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                cartData.push(data);
+            })
+            .catch((error) => console.error("Error fetching course data:", error));
+    }
+    return cartData;
 }
+
+module.exports = getCourseData;
+
+
+// let term = 'fall';
+// let courses = ['cmput 101', 'cmput 174'];
+// getCourseData(term, courses);
--- utils/getScheduleVariations.js
+++ utils/getScheduleVariations.js
@@ -1,41 +1,24 @@
-export default function scheduleCombinator( term, courses ) {
-    // MONGO_URI=mongodb+srv://tylerallenbeach:secret123@schedule-builder.q4azy.mongodb.net/schedule-builder?retryWrites=true&w=majority&appName=schedule-builder
-    let courses = ['cmput 101']
-    const mongodb = require('mongodb');
-    const MongoClient = mongodb.MongoClient;
+const getCourseData = require('./getDataForEachCourse.js');
 
-    const uri = process.env.MONGO_URI;
-
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    client.connect(err => {
-        const collection = client.db("schedule-builder").collection("courses");
-        // get the course data from the database
-        // for each course, get the course data from the database
-
-
-        const courseData = courses.map(course => {
-            collection.findOne({ term: course.term }, (err, result) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(result);
-                }
-            });
-        });
-
-        console.log(courseData);
-
-       
-       
-       
-       
-       
-       
-       
-       
-        client.close();
-    });
-
-
+async function scheduleCombinator(term, courses) {
+    try {
+        let courseData = await getCourseData(term, courses);  
+        let scheduleVariations = [];
+        
+        courseData.forEach(course => {
+        
+        
+        })
+        
+        return scheduleVariations;
+    } catch (error) {
+        console.error("Error in scheduleCombinator:", error);
+    }
 }
+
+let term = 'fall';
+let courses = ['cmput 174'];
+
+scheduleCombinator(term, courses).then(scheduleVariations => {
+    console.log("Schedule Variations:", scheduleVariations);
+});
